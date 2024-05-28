e3e321b8d9e85757196fa1d8a1f36b210acbdb25
Jonas Venzel <138597036+JonasVenzel@users.noreply.github.com>
2024-05-28 16:49:50 -0300

    Add files via upload

---
+++ Tentativa6/index.js
@@ -0,0 +1,56 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+
+
+/*=============== CARROSSEL DE IMAGENS ===============*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for ( i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += "active";
+
+}
+
+/*=============== NOME DA PÁGINA ===============*/
+
+let docTitile = document.title;
+window.addEventListener("blur", () =>{
+    document.title = "Volte Aqui 😠";
+})
+window.addEventListener("focus", () =>{
+    document.title = docTitile;
+})
