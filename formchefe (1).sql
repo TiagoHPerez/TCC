9e3a1656de0a95223630da17a4c4f78324a0e364
Tiago <138597047+TiagoHPerez@users.noreply.github.com>
2024-10-06 21:47:54 -0300

    Update formchefe.sql

--- formchefe (1).sql
+++ formchefe (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30/09/2024 às 04:08
+-- Tempo de geração: 07/10/2024 às 02:50
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -42,7 +42,10 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `name`, `ingredientes`, `preparo`, `obs`, `image`) VALUES
 (40, 'Gatinho Gominola', '1 gato gominolo', 'assar o gato gominolo em temperaturas elevadas para ele ficar queimado', 'Um belo gato gominolo que serve até 1 pessoa', 'imagem_2024-05-10_232249646-removebg-preview.png'),
-(43, 'a', 'b', 'c', 'd', 'imagem_2024-06-20_230323292-removebg-preview.png');
+(43, 'a', 'b', 'c', 'e', 'imagem_2024-06-20_230323292-removebg-preview.png'),
+(44, 'Aniquilação total da vida inferior', 'gás mostarda', 'espalhe gentilmente o gás mostarda por toda uma área determinada e aguarde seus efeitos.', 'como matar todos os mosquitos de uma maneira simples e rápida! ;)', 'imagem_2024-10-06_171337557.png'),
+(46, 'q', 'q', 'q', 'q', 'imagem_2024-10-06_172633189.png'),
+(47, 'a', 'a', 'a', 'a', 'album_2024-06-08_22-41-33.png');
 
 -- --------------------------------------------------------
 
@@ -54,16 +57,17 @@ CREATE TABLE `usuarios` (
   `id` int(255) NOT NULL,
   `fullnome` varchar(110) NOT NULL,
   `usuario` varchar(45) NOT NULL,
-  `senha` varchar(45) NOT NULL
+  `senha` varchar(45) NOT NULL,
+  `adm` varchar(3) NOT NULL DEFAULT 'Não'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `fullnome`, `usuario`, `senha`) VALUES
-(6, 'q', 'q', 'q'),
-(7, 'tiago henrique perez', 'tagostoso', 'bigtetas04');
+INSERT INTO `usuarios` (`id`, `fullnome`, `usuario`, `senha`, `adm`) VALUES
+(6, 'q', 'q', 'q', 'Sim'),
+(7, 'tiago henrique perez', 'tagostoso', 'tiago', 'Não');
 
 --
 -- Índices para tabelas despejadas
@@ -90,13 +94,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
