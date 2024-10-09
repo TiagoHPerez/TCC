3804a5777b2aae8f7389994d168fadbbd681ff2a
Tiago <138597047+TiagoHPerez@users.noreply.github.com>
2024-10-08 23:40:19 -0300

    Update formchefe (1).sql

--- formchefe (1).sql
+++ formchefe (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/10/2024 às 02:50
+-- Tempo de geração: 09/10/2024 às 04:42
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -32,7 +32,7 @@ CREATE TABLE `products` (
   `name` varchar(255) NOT NULL,
   `ingredientes` text NOT NULL,
   `preparo` text NOT NULL,
-  `obs` varchar(255) NOT NULL,
+  `obs` text NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -41,11 +41,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `ingredientes`, `preparo`, `obs`, `image`) VALUES
-(40, 'Gatinho Gominola', '1 gato gominolo', 'assar o gato gominolo em temperaturas elevadas para ele ficar queimado', 'Um belo gato gominolo que serve até 1 pessoa', 'imagem_2024-05-10_232249646-removebg-preview.png'),
-(43, 'a', 'b', 'c', 'e', 'imagem_2024-06-20_230323292-removebg-preview.png'),
+(40, 'Gatinho Gominola', '1 gato gominolo 2 gatos gominolos', 'assar o gato gominolo em temperaturas elevadas para ele ficar queimado', 'Um belo gato gominolo que serve até 1 pessoa', 'imagem_2024-05-10_232249646-removebg-preview.png'),
+(43, 'a', 'b', 'c', 'e', 'imagem_2024-10-08_203051428.png'),
 (44, 'Aniquilação total da vida inferior', 'gás mostarda', 'espalhe gentilmente o gás mostarda por toda uma área determinada e aguarde seus efeitos.', 'como matar todos os mosquitos de uma maneira simples e rápida! ;)', 'imagem_2024-10-06_171337557.png'),
-(46, 'q', 'q', 'q', 'q', 'imagem_2024-10-06_172633189.png'),
-(47, 'a', 'a', 'a', 'a', 'album_2024-06-08_22-41-33.png');
+(47, 'Alien gato a milanesa', 'alien, gato e 1/2 colheres de milanesa', 'misture o alien e o gato, asse no forno por 30 minutos a 180 graus e jogue a milanesa', 'gato a milanesa delicioso', 'album_2024-06-08_22-41-33.png'),
+(48, 'comida de lanchonete', 'lanchonete', 'pede pra garçonete', 'comida de lanchonete facil e rapido e baratete', 'imagem_2024-10-08_203148044.png'),
+(50, 'Bolo Simples', '2 xícaras (chá) de açúcar farinha de trigo 3 xícaras (chá) de farinha de trigo margarina 4 colheres (sopa) de margarina ovo 3 ovos leite 1 e 1/2 xícara (chá) de leite fermento em pó químico 1 colher (sopa) bem cheia de fermento em pó', '1 Bata as claras em neve e reserve.  2 Misture as gemas, a margarina e o açúcar até obter uma massa homogênea.  3 Acrescente o leite e a farinha de trigo aos poucos, sem parar de bater.  4 Por último, adicione as claras em neve e o fermento.  5 Despeje a massa em uma forma grande de furo central untada e enfarinhada.  6 Asse em forno médio 180 °C, preaquecido, por aproximadamente 40 minutos ou ao furar o bolo com um garfo, este saia limpo.', 'Um bolo simples fofinho e quentinho com uma xícara de café pode ser tudo o que você precisa numa tarde chuvosa. E essa aqui é a receita que pode dar isso para você. Essa receita é bem simples e não leva muitos ingredientes. A massa branca pode ser usada com aa a a a a a a  a asyuibwefuybfweua fahuyfbawuyhfaw fyuawfbwofyuafbawyh bfaw', 'imagem_2024-10-08_205257112.png'),
+(53, 'batata frita', '1 gato gominolo', 'assar o gato gominolo em temperaturas elevadas para ele ficar queimado', 'a', '1000_F_707782560_xUQ9VvZ4vlA6hzHul4eWeM5iwNZr9qQr-removebg-preview.png');
 
 -- --------------------------------------------------------
 
@@ -67,7 +69,8 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `fullnome`, `usuario`, `senha`, `adm`) VALUES
 (6, 'q', 'q', 'q', 'Sim'),
-(7, 'tiago henrique perez', 'tagostoso', 'tiago', 'Não');
+(7, 'tiago henrique perez', 'tagostoso', 'tiago', 'Não'),
+(9, 'juninho', 'juninhogameplay', '123', 'Sim');
 
 --
 -- Índices para tabelas despejadas
@@ -94,13 +97,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
