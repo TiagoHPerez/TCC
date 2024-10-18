cf50791574a2ef1d3efde9ab4d087cb745aecca3
Tiago <138597047+TiagoHPerez@users.noreply.github.com>
2024-10-17 21:46:19 -0300

    Update formchefe.sql

--- formchefe.sql
+++ formchefe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17/10/2024 às 00:57
+-- Tempo de geração: 18/10/2024 às 02:48
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -24,6 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `products`
 --
 
@@ -38,8 +50,47 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Despejando dados para a tabela `products`
+--
+
+INSERT INTO `products` (`id`, `user_id`, `name`, `ingredientes`, `preparo`, `obs`, `image`) VALUES
+(92, 7, 'Brownie de Caneca', '2 Colheres (sopa) de margarina (com ou sem sal, tanto faz), 1 e 1/2 colher (sopa) de açúcar, 2 e 1/2 colheres (sopa) de Nescau, 2 e 1/2 colheres (sopa) de farinha de trigo e 2 colheres (sopa) de água.', 'Derreta as 2 colheres de margarina na caneca, coloque as colheres de açúcar e Nescau e misture, logo após coloque a farinha e água, neste momento, se você quiser pode colocar algum recheio, como frutas, chocolate picado ou outras coisas, logo após leve para o micro-ondas por 1 minuto e meio, ou até ficar firme.', 'Um delicioso brownie de caneca feito com Nescau e pouquíssimos ingredientes, fácil e rápido!', 'brownie caneca.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `fullnome` varchar(110) NOT NULL,
+  `usuario` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `adm` varchar(3) NOT NULL DEFAULT 'Não'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `fullnome`, `usuario`, `senha`, `adm`) VALUES
+(1, 'q', 'q', 'q', 'Sim'),
+(6, 'a', 'a', 'a', 'Não'),
+(7, 'admin', 'admin', 'admin', 'Sim'),
+(8, 'b', 'b', 'b', 'Não');
+
+--
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `favoritos_ibfk_1` (`user_id`),
+  ADD KEY `favoritos_ibfk_2` (`product_id`);
 
 --
 -- Índices de tabela `products`
@@ -49,18 +100,44 @@ ALTER TABLE `products`
   ADD KEY `idx_user_id` (`user_id`);
 
 --
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de tabela `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restrições para tabelas despejadas
 --
+
+--
+-- Restrições para tabelas `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Restrições para tabelas `products`
